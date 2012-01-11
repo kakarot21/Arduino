a19054ce0a0cbad518d7db5b9dff48e954153ba6
Sinan Kaya <sinankaya21@hotmail.com>
2012-01-11 01:47:48 +0100

    Verlinkung der Headerdateien funktioniert muehsam

--- testbench/testbench.ino
+++ testbench/testbench.ino
@@ -1,20 +1,16 @@
+#include "light_sensor.h"
 
 //#include "C:\Users\Kakarot\Desktop\Project\light_sensor\light_sensor.h"
-#include "light_sensor.h"
 //#include "C:\Users\Kakarot\Desktop\Project\blink_LED\blink_LED.h"
-//#include "../blink_LED/blink_LED.h"
 
-int temp;
 
+int temp = 5;
 void setup() {
-  //blink_setup(temp1);
 
 }
 
 void loop() {
-  temp = calc_light(5);
-  //blink_led(temp2);
-  //pinMode(1,OUTPUT);
+   calc_light(temp);
 }
 
 
